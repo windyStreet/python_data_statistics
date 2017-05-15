0fd629c422d8ff15c14dc26b53a942d668108cdc
windyStreet <yq904276384@foxmail.com>
2017-05-15 15:57:11 +0800

    DBCODE init

--- bin/until/DBCODE.py
+++ bin/until/DBCODE.py
@@ -1,2 +1,23 @@
-eq=11
-neq=21
+EQ = 11  # 相等
+NE = 21  # 不相等
+LT = 12  # 小于
+GT = 13  # 大于
+LTE = 121  # 小于等于
+GTE = 131  # 大于等于
+IN = 14  # in
+NIN = 24  # not in
+OR = 15  # 或者
+AND = 16  # 并且
+NOT = 17  # 取反
+
+RELATION_EQ = "$eq"
+RELATION_NE = "$ne"
+RELATION_IN = "$in"
+RELATION_NIN = "$nin"
+RELATION_LT = "$lt"
+RELATION_LTE = "$lte"
+RELATION_GT = "$gt"
+RELATION_GTE = "$gte"
+RELATION_NOT = "$not"
+RELATION_OR = "$or"
+
